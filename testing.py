7a4a0cbb299eb98dd34493e650df979f36685636
YeshwanthKare <yesh.kare@gmail.com>
2024-09-08 15:40:16 +0200

    testing new lines

--- testing.py
+++ testing.py
@@ -1,8 +1,16 @@
+"""
+This module demonstrates a simple greeting function.
+"""
+
 def hello():
     """
-    this is demo
+    Returns a greeting message.
+
+    This function is a simple example for demo purposes.
+    It returns the string 'Hello Dosto' when called.
     """
     return "Hello Dosto"
 
 
+# Calling the hello function to print the greeting
 hello()
